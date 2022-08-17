e14eb6d30c5d49543cfdec6b94493fbaebac3b67
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2022-08-17 18:04:37 +0500

    data

--- src/components/Main/category/Category.jsx
+++ src/components/Main/category/Category.jsx
@@ -1,28 +1,38 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-
+import React from 'react'
+import './category.css'
 export default function Category() {
-	const [ posts, setPosts ] = useState([]);
-
-	function Bum() {
-		useEffect(() => {
-			fetch('https://jsonplaceholder.typicode.com/photos').then((response) => response.json()).then((data) => {
-				setPosts(data); // new
-			});
-		}, []);
-	}
-
 	return (
 		<div>
-			{posts.length > 0 ? (
-				posts.map((item) => (
-					<div>
-						<img src={item.url} />
-					</div>
-				))
-			) : (
-				<h1 onClick={Bum}>Loading posts...</h1>
-			)}
+			<div className="top">
+				<h2>Популярные категории</h2>
+				<button>все категории</button>
+			</div>
+			<div className="boxs">
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+				<div className="box">
+					Ковровая плитка для офиса
+				</div>
+			</div>
 		</div>
-	);
+	)
 }
